5d2b3cd7cf075145cc7c24e2fe8b1452cb738efd
doniatung <donia.tung@dali.dartmouth.edu>
2020-04-18 20:09:46 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-const counter = () => {
-  let num = 0;
-  setInterval(() => {
-    num += 1;
-    $('#main').html(`you've been on this page for ${num} seconds!`);
-  }, 1000);
-};
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-counter();
+ReactDOM.render(<App />, document.getElementById('main'));
